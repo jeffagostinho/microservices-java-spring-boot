4a2a8592fc8fe8c8c7329f000793481ab5fbb9f2
Jefferson Agostinho <jefferson.andrade.agostinho@gmail.com>
2025-04-05 11:35:53 -0300

    circuit-breaker with resilience4j

---
+++ book-service/src/main/java/br/com/jeffagostinho/controller/FooBarController.java
@@ -0,0 +1,38 @@
+package br.com.jeffagostinho.controller;
+
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/books")
+public class FooBarController {
+
+    private final Logger logger = LoggerFactory.getLogger(FooBarController.class);
+
+    @GetMapping(value = "/foo-bar")
+//    @Retry(name = "foo-bar", fallbackMethod = "fallbackFooBar")
+//    @CircuitBreaker(name = "default", fallbackMethod = "fallbackFooBar")
+//    @RateLimiter(name = "default")
+    @Bulkhead(name = "default")
+    public String fooBar()  {
+        logger.info("Request foo bar!");
+//        var response = new RestTemplate()
+//                .getForEntity("http://localhost:8080/foo-bar", String.class);
+//        return response.getBody();
+
+        return "foo-bar";
+    }
+
+    public String fallbackFooBar(Exception e) {
+        logger.info("Fallback method called because of {}", e.getMessage());
+        return "Fallback Foo Bar!!!";
+    }
+}
